2c8bac3671f4762be9ef6f0054b52797662cbd19
Raj <patelraj95@gmail.com>
2020-05-16 16:42:48 +0530

    Update Twopointers.cpp

--- Leetcode/142_Linked List Cycle II/Twopointers.cpp
+++ Leetcode/142_Linked List Cycle II/Twopointers.cpp
@@ -1,1 +1,38 @@
-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+private:
+    ListNode* cycleStart(ListNode* slow, ListNode* fast, ListNode* head){
+        slow = head;
+        
+        while(slow != fast){
+            slow = slow->next;
+            fast = fast->next;
+        }
+        return slow;
+    }
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        
+        while( (fast != NULL) && (slow != NULL) ){
+            if( slow->next == NULL || fast->next == NULL || fast->next->next == NULL ){
+                return NULL;
+            }
+            slow = slow->next;
+            fast = fast->next->next;
+            if( slow == fast ){
+                return cycleStart(slow, fast, head);
+            }
+        }
+        
+        return NULL;
+    }
+};
