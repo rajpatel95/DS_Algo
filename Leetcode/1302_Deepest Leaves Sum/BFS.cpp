af3cf3f99b5f1acabc6fe60d60f7768ed882cb5e
Raj <patelraj95@gmail.com>
2021-04-07 00:23:24 +0530

    Create BFS.cpp

---
+++ Leetcode/1302_Deepest Leaves Sum/BFS.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int deepestLeavesSum(TreeNode* root) {
+        queue<TreeNode*> q;
+        
+        q.push(root);
+        int ans;
+        
+        while(! q.empty() ){
+            int q_size = q.size();
+            ans = 0;
+            
+            while(q_size--){
+                TreeNode* node = q.front();
+                q.pop();
+                
+                ans += node->val;
+                if(node->left != NULL){
+                    q.push(node->left);
+                }
+                if(node->right != NULL){
+                    q.push(node->right);
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
