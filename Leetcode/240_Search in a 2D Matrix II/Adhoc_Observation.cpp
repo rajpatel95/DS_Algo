ae5d42f61c324f971c8876239fffe0c1c6bda1ab
Raj <patelraj95@gmail.com>
2021-05-15 23:02:20 +0530

    Create Adhoc_Observation.cpp

---
+++ Leetcode/240_Search in a 2D Matrix II/Adhoc_Observation.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row = 0, col = matrix[0].size() - 1;
+        
+        while(row >= 0 && row < matrix.size() && col >= 0 && col < matrix[0].size()){
+            if(matrix[row][col] == target){
+                return true;
+            }
+            
+            else if( matrix[row][col] > target){
+                col--;
+            }  
+            
+            else{
+                row++;
+            }
+        }
+        
+        return false;
+    }
+};
